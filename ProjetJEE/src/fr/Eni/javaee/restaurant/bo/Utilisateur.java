5b17ad159d9795e6da667694574c6e19cafc3df0
Administrateur <Administrateur@138-UC51-08.ad.campus-eni.fr>
2019-07-30 14:29:52 +0200

    remember the name

--- ProjetJEE/src/fr/Eni/javaee/restaurant/bo/Utilisateur.java
+++ ProjetJEE/src/fr/Eni/javaee/restaurant/bo/Utilisateur.java
@@ -1,6 +1,10 @@
 package fr.Eni.javaee.restaurant.bo;
 
 public class Utilisateur {
+
+	public Utilisateur(Utilisateur selectUtilisateurByIdUtilisateur) {
+		// TODO Auto-generated constructor stub
+	}
 	
 	
 
--- ProjetJEE/src/fr/Eni/javaee/restaurant/servlet/ServletdeConnexion.java
+++ ProjetJEE/src/fr/Eni/javaee/restaurant/servlet/ServletdeConnexion.java
@@ -9,12 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.Eni.javaee.restaurant.BusinessException;
+import fr.Eni.javaee.restaurant.bll.UtilisateurManager;
+import fr.Eni.javaee.restaurant.bo.Utilisateur;
+
 /**
  * Servlet implementation class ServletConnexion
  */
 @WebServlet("/ServletdeConnexion")
 public class ServletdeConnexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	int tentative=0;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -23,15 +28,53 @@ public class ServletdeConnexion extends HttpServlet {
         super();
         
     }
+    
+    @Override
+    public void init() throws ServletException {
+    	tentative=0;
+    	super.init();
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	String mail = null;
+	String mot_de_passe = null;
+	
+	UtilisateurManager utilisateurmanager = new UtilisateurManager();
+	
+	if (utilisateurmanager.getUtilisateur(mail, mot_de_passe) == -1)
+	{
+//		int i=0;
 		String error = null;
-		// si erreur
+		// il ya erreur
 		error = "il y a une erreur";		
 		request.setAttribute("error", error);
+		tentative++;
+		if(tentative==3) {
+			tentative=0;
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/inscription.jsp");
+			rd.forward(request, response);
+		}
+
+		//erreur
+	}else
+	{
+		int idUtilisateur = 0;
+		//ok
+		try {
+			Utilisateur utilisateur = new Utilisateur(utilisateurmanager.selectUtilisateurByIdUtilisateur(idUtilisateur));
+		} catch (BusinessException e) {
+			
+			e.printStackTrace();
+		}
+	}
+		 
+			
+			
+		
+	
 		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/connection.jsp");
 		rd.forward(request, response);
